87371f22158c347bc3e60b1f316a501202e3b366
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-09-17 17:36:25 +0000

    finished setting up homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,19 @@ class PagesController < ApplicationController
   end
 
   def rock
+    @player_move = "rock"
 
     render({ :template => "pages_templates/rock" })
   end
 
   def paper
+    @player_move = "paper"
 
     render({ :template => "pages_templates/paper" })
   end
 
   def scissors
+    @player_move = "scissors"
 
     render({ :template => "pages_templates/scissors" })
   end
