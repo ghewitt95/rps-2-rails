2399453808d24f72733d7b4e95d8602e492fe739
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-09-17 16:02:13 +0000

    created routes for all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  # get("/your_first_screen", { :controller => "pages", :action => "first" })
+  get("/", { :controller => "pages", :action => "home" })
+  get("/rock", { :controller => "pages", :action => "rock" })
+  get("/paper", { :controller => "pages", :action => "paper" })
+  get("/scissors", { :controller => "pages", :action => "scissors" })
 end
