6995d7461f52b4a0fee3efaa979c30d4754e3187
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-09-17 19:23:18 +0000

    created layouts for all pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,18 +6,39 @@ class PagesController < ApplicationController
 
   def rock
     @player_move = "rock"
+    @random_move = ["rock", "paper", "scissors"].sample
+    @outcome = winner(@player_move, @random_move)
 
     render({ :template => "pages_templates/rock" })
   end
 
   def paper
     @player_move = "paper"
+    @random_move = ["rock", "paper", "scissors"].sample
+    @outcome = winner(@player_move, @random_move)
 
     render({ :template => "pages_templates/paper" })
   end
 
   def scissors
     @player_move = "scissors"
+    @random_move = ["rock", "paper", "scissors"].sample
+    @outcome = winner(@player_move, @random_move)
 
     render({ :template => "pages_templates/scissors" })
   end
+
+  private
+
+  def winner(player, computer)
+    if player == computer
+      "We tied!"
+    elsif (player == "rock" && computer == "scissors") ||
+          (player == "paper" && computer == "rock") ||
+          (player == "scissors" && computer == "paper")
+      "We won!"
+    else
+      "We lost"
+    end
+  end
+end
