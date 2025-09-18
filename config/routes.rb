adc4527327c7cc721295777126be2a1a9dde2d32
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-09-18 00:31:24 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "home" })
+
   get("/rock", { :controller => "pages", :action => "rock" })
+
   get("/paper", { :controller => "pages", :action => "paper" })
+  
   get("/scissors", { :controller => "pages", :action => "scissors" })
 end
